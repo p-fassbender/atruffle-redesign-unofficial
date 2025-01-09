89f8e61ee094849022e9e235e018b843ef525adc
Preston Fassbender <fassbenderp0551@gmail.com>
2025-01-09 12:34:34 -0600

    react hook form installed and started brainstorming the contact page form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,33 @@
 import { Stack, Typography } from '@mui/material'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export const Contact = () => {
+    /**
+     * form built with react hook form
+     * form for sending an email with emailjs
+     * subject line is prefilled with dropbox
+     * dropbox: place order, ask question, other
+     * body default value set based on watching the subject line
+     * body: name, phone, email, order details
+     */
+    const form = useForm({
+        defaultValues: {
+            emailTo: 'user983176@atruffle.com',
+            subjectLine: '',
+            body: '',
+            name: '',
+            emailFrom: '',
+            phoneNumber: '',
+            currentDate: new Date().toLocaleDateString()
+        },
+        // mode: 'onSubmit'
+        mode: 'onBlur'
+        // mode: 'onTouched'
+        // mode: 'onChange'
+        // mode: 'all'
+    })
+
     return (
         <Stack
             spacing={2}
@@ -14,12 +40,7 @@ export const Contact = () => {
         >
             <Typography>
                 Contact
-                form built with react hook form
-                form for sending an email with emailjs
-                subject line is prefilled with dropbox
-                    dropbox: place order, ask question, other
-                body default value set based on watching the subject line
-                    body: name, phone, email, order details
+
             </Typography>
         </Stack>
     )
